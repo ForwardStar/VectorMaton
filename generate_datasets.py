36620ffe934b3156d4c9f49de6c2d02a1f5998c3
ForwardStar <haoxuanxie@link.cuhk.edu.cn>
2025-09-01 18:39:27 +0800

    Update README and datasets

--- generate_datasets.py
+++ generate_datasets.py
@@ -5,6 +5,7 @@ from datasets import load_dataset
 from transformers import AutoTokenizer, AutoModel
 import torch
 import numpy as np
+import time
 
 class Logger:
     LEVELS = {
@@ -36,13 +37,30 @@ class Logger:
     def error(self, message):
         self._log("ERROR", message)
 
+def format_time(seconds: float) -> str:
+    seconds = int(seconds)
+    if seconds >= 3600:  # 1 hour
+        h = seconds // 3600
+        m = (seconds % 3600) // 60
+        s = seconds % 60
+        return f"{h}h {m}min {s}s"
+    elif seconds >= 60:  # 1 minute
+        m = seconds // 60
+        s = seconds % 60
+        return f"{m}min {s}s"
+    else:
+        return f"{seconds}s"
+
 log = Logger(level="INFO")  # set threshold level
 if not os.path.exists("datasets"):
     os.makedirs("datasets")
 if not os.path.exists("datasets/code_search_net"):
     os.makedirs("datasets/code_search_net")
     log.info("Downloading and generating CodeSearchNet dataset...")
-    dataset = load_dataset("code_search_net", "python", trust_remote_code=True)  # pick a language, e.g. python, java, go, ruby, etc.
+    start = time.perf_counter()
+    # Load dataset
+    dataset = load_dataset("code_search_net", "python", trust_remote_code=True)
+    log.info(f"Dataset loaded. Size = {dataset['train'].num_rows} entries.")
     # Convert dataset to required format
     with open("datasets/code_search_net/vectors.txt", "w") as vec_file, open("datasets/code_search_net/strings.txt", "w") as str_file:
         tokenizer = AutoTokenizer.from_pretrained("microsoft/codebert-base")
@@ -56,5 +74,8 @@ if not os.path.exists("datasets/code_search_net"):
             str_file.write(s + "\n")
             vec_file.write(" ".join(map(str, v)) + "\n")
     log.info("CodeSearchNet dataset downloaded and processed!")
+    end = time.perf_counter()
+    elapsed = end - start
+    log.info(f"Time consumption: {format_time(elapsed)}")
 else:
     log.info("CodeSearchNet dataset already exists. Skipped.")
