a8b50af4f75ea7630a162d6980cd3f09f7cbdf0c
ForwardStar <haoxuanxie@link.cuhk.edu.cn>
2025-09-17 17:13:43 +0800

    Minor optimization

--- source/vector_db.cpp
+++ source/vector_db.cpp
@@ -28,13 +28,23 @@ void VectorDB::build() {
                 auto tmp = new NSW(*nsws[gsa.st[largest_state].hash_value]);
                 nsws[st.hash_value] = tmp;
                 // Add vectors that are in st.ids but not in the copied NSW
-                std::unordered_map<uint32_t, bool> existing;
-                for (auto id : gsa.st[largest_state].ids) {
-                    existing[id] = true;
-                }
-                for (auto id : st.ids) {
-                    if (existing.find(id) == existing.end()) {
-                        tmp->insert(id);
+                int l = 0, r = 0;
+                while (l < st.ids.size() || r < gsa.st[largest_state].ids.size()) {
+                    if (r == gsa.st[largest_state].ids.size()) {
+                        tmp->insert(st.ids[l]);
+                        l++;
+                    } else if (l == st.ids.size()) {
+                        // Should not happen
+                        LOG_WARN("Unexpected condition in VectorDB::build(): child state has ids not in parent state");
+                        break;
+                    } else if (st.ids[l] < gsa.st[largest_state].ids[r]) {
+                        tmp->insert(st.ids[l]);
+                        l++;
+                    } else if (st.ids[l] > gsa.st[largest_state].ids[r]) {
+                        r++;
+                    } else {
+                        l++;
+                        r++;
                     }
                 }
             } else {
